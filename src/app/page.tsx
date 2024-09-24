2d0cf59cc87742c9dce8cf309e253219173f0130
Precious Adeyinka <pflash90@gmail.com>
2024-09-25 00:21:09 +0100

    making it responsive by removing fixed height layout on mobile (viewport)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full overflow-hidden p-3 bg-gray-50 flex items-center justify-center">
+    <div className="h-auto md:h-screen w-full overflow-hidden p-3 bg-gray-50 flex items-center justify-center">
       <Dashboard />
     </div>
   );
